72d4a4faaac094b24d5a9ee9c03141c211ea6cb5
eduarmreyes <eduar.mreyes@gmail.com>
2019-08-25 21:28:08 -0400

    add page information and start navigation updates

--- src/components/search.js
+++ src/components/search.js
@@ -1,15 +1,18 @@
 /** @jsx jsx */
 import React from "react";
 import { css, jsx } from "@emotion/core";
+import { Link } from "react-router-dom";
 
 import FormSearch from "./_form_search";
 import Results from "./results";
 
 class Search extends React.Component {
-  state = { searchTerm: "", userData: {} };
+  state = { searchTerm: "", userData: {}, currentPage: 0, totalPages: 0 };
 
-  async getUserData(searchTerm) {
-    return await fetch(`https://api.github.com/search/users?q=${searchTerm}`)
+  async getUserData(searchTerm, page = 1) {
+    return await fetch(
+      `https://api.github.com/search/users?q=${searchTerm}&page=${page}`
+    )
       .then(data => {
         return data.json();
       })
@@ -20,7 +23,12 @@ class Search extends React.Component {
 
   componentDidMount() {
     this.getUserData(this.props.searchTerm).then(data => {
-      this.setState({ userData: data });
+      const totalPages = data.total_count / 30;
+      this.setState({
+        userData: data,
+        currentPage: 1,
+        totalPages: Math.ceil(Number(totalPages)),
+      });
     });
   }
 
@@ -32,6 +40,26 @@ class Search extends React.Component {
             border-top: 2rem solid #333;
           `}
         >
+          <nav
+            css={css`
+              max-width: 1040px;
+              margin: 1rem auto;
+            `}
+          >
+            <Link
+              css={css`
+                color: #007eff;
+                -webkit-text-decoration: none;
+                text-decoration: none;
+              `}
+              to="/"
+            >
+              <span role="img" aria-label="back">
+                👈
+              </span>{" "}
+              Home
+            </Link>
+          </nav>
           <form
             onSubmit={this.handleSearch}
             css={css`
@@ -51,17 +79,151 @@ class Search extends React.Component {
             margin: auto;
           `}
         >
-          <h3>
-            {Boolean(this.state.userData.total_count)
-              ? new Intl.NumberFormat("en-US").format(
-                  this.state.userData.total_count
-                )
-              : 0}{" "}
-            users
-          </h3>
+          <div
+            css={css`
+              display: flex;
+              align-items: center;
+              justify-content: space-between;
+            `}
+          >
+            <div
+              css={css`
+                display: flex;
+                align-items: center;
+              `}
+            >
+              <h3
+                css={css`
+                  padding-right: 1.5rem;
+                `}
+              >
+                <span
+                  css={css`
+                    color: #6c757d;
+                  `}
+                >
+                  {Boolean(this.state.userData.total_count)
+                    ? new Intl.NumberFormat("en-US").format(
+                        this.state.userData.total_count
+                      )
+                    : 0}{" "}
+                </span>
+                users
+              </h3>
+              {" | "}
+              <p
+                css={css`
+                  padding-left: 1.5rem;
+                  color: #6c757d;
+                `}
+              >
+                Page{" "}
+                <span
+                  css={css`
+                    font-weight: 700;
+                    padding-right: 0.2rem;
+                  `}
+                >
+                  {Boolean(this.state.currentPage)
+                    ? new Intl.NumberFormat("en-US").format(
+                        this.state.currentPage
+                      )
+                    : 0}
+                </span>
+                /
+                <span
+                  css={css`
+                    font-weight: 700;
+                    padding-left: 0.2rem;
+                  `}
+                >
+                  {Boolean(this.state.totalPages)
+                    ? new Intl.NumberFormat("en-US").format(
+                        this.state.totalPages
+                      )
+                    : 0}
+                </span>
+              </p>
+            </div>
+            <div>
+              <button
+                css={css`
+                  appearance: none;
+                  background-position: -1px -1px;
+                  background-repeat: repeat-x;
+                  background-size: 110% 110%;
+                  border: 1px solid rgba(27, 31, 35, 0.2);
+                  border-radius: 0.25em;
+                  cursor: pointer;
+                  display: inline-block;
+                  font-size: 14px;
+                  font-weight: 600;
+                  line-height: 20px;
+                  padding: 6px 12px;
+                  position: relative;
+                  user-select: none;
+                  vertical-align: middle;
+                  white-space: nowrap;
+                  background-color: #eff3f6;
+                  background-image: linear-gradient(
+                    -180deg,
+                    #fafbfc,
+                    #eff3f6 90%
+                  );
+                  margin-right: 1rem;
+                  color: #24292e;
+                  &:active,
+                  &:focus {
+                    outline: 1px solid hotpink;
+                  }
+                `}
+              >
+                <span role="img" aria-label="hand indicating to the left">
+                  👈
+                </span>{" "}
+                Previous page
+              </button>
+              <button
+                css={css`
+                  appearance: none;
+                  background-position: -1px -1px;
+                  background-repeat: repeat-x;
+                  background-size: 110% 110%;
+                  border: 1px solid rgba(27, 31, 35, 0.2);
+                  border-radius: 0.25em;
+                  cursor: pointer;
+                  display: inline-block;
+                  font-size: 14px;
+                  font-weight: 600;
+                  line-height: 20px;
+                  padding: 6px 12px;
+                  position: relative;
+                  user-select: none;
+                  vertical-align: middle;
+                  white-space: nowrap;
+                  background-color: #eff3f6;
+                  background-image: linear-gradient(
+                    -180deg,
+                    #fafbfc,
+                    #eff3f6 90%
+                  );
+                  color: #24292e;
+                  &:active,
+                  &:focus {
+                    outline: 1px solid hotpink;
+                  }
+                `}
+              >
+                Next page{" "}
+                <span role="img" aria-label="hand indicating to the right">
+                  👉
+                </span>
+              </button>
+            </div>
+          </div>
           {Boolean(this.state.userData.items) &&
             this.state.userData.items.map(user => {
-              return <Results user={user} />;
+              return <Results key={user.node_id} user={user} />;
             })}
         </div>
       </div>
